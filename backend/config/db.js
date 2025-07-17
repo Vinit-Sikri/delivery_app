4bd8fe2d9ec6a854c9886fd8db242f320dfc5fac
Vinit Sikri <vinitsikri155@gmail.com>
2025-07-17 23:35:47 +0530

    Final fix: Remove tracked node_modules

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-  await mongoose.connect('mongodb+srv://vinit:87654321@cluster0.3dbj6uk.mongodb.net/zomato').then(()=>console.log("DB connected"))
+  await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB connected"))
 }
